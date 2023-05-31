a1c709212a27766a5202665558194afa3eafc9f4
Arief Fauzi <arieff75@gmail.com>
2023-05-31 14:13:48 +0700

    Add project files.

---
+++ FaceONNX/IFaceDetector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+
+namespace FaceONNX
+{
+    /// <summary>
+    /// Defines face detector interface.
+    /// </summary>
+    public interface IFaceDetector : IDisposable
+    {
+        #region Interface
+
+        /// <summary>
+        /// Gets or sets confidence threshold.
+        /// </summary>
+        float ConfidenceThreshold { get; set; }
+
+        /// <summary>
+        /// Gets or sets NonMaxSuppression threshold.
+        /// </summary>
+        float NmsThreshold { get; set; }
+
+        /// <summary>
+        /// Returns face detection results.
+        /// </summary>
+        /// <param name="image">Bitmap</param>
+        /// <returns>Rectangles</returns>
+        Rectangle[] Forward(Bitmap image);
+
+        /// <summary>
+        /// Returns face detection results.
+        /// </summary>
+        /// <param name="image">Image in BGR terms</param>
+        /// <returns>Rectangles</returns>
+        Rectangle[] Forward(float[][,] image);
+
+        #endregion
+    }
+}
---
+++ FaceONNX/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using UMapx.Visualization;
+
+namespace FaceONNX
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("FaceONNX: Face detection");
+            var files = Directory.GetFiles(@"..\..\..\images", "*.*", SearchOption.AllDirectories);
+            var path = @"..\..\..\results";
+            Directory.CreateDirectory(path);
+
+            using var faceDetector = new FaceDetector(0.95f, 0.5f);
+            using var painter = new Painter()
+            {
+                BoxPen = new Pen(Color.Yellow, 4),
+                Transparency = 0,
+            };
+
+            Console.WriteLine($"Processing {files.Length} images");
+
+            foreach (var file in files)
+            {
+                using var bitmap = new Bitmap(file);
+                var output = faceDetector.Forward(bitmap);
+
+                foreach (var rectangle in output)
+                {
+                    var paintData = new PaintData()
+                    {
+                        Rectangle = rectangle,
+                        Title = string.Empty
+                    };
+                    using var graphics = Graphics.FromImage(bitmap);
+                    painter.Draw(graphics, paintData);
+                }
+
+                var filename = Path.GetFileName(file);
+                bitmap.Save(Path.Combine(path, filename));
+                Console.WriteLine($"Image: [{filename}] --> detected [{output.Length}] faces");
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
+    
